602d78488327f5fe394379f1bf6eaea8001fe948
Vincent Rischmann <vincent@rischmann.fr>
2023-05-29 08:36:56 +0200

    update build for latest zig

--- build.zig
+++ build.zig
@@ -6,27 +6,34 @@ pub fn build(b: *std.build.Builder) !void {
     if (target_info.target.os.tag == .linux and target_info.target.abi == .gnu) {
         target.setGnuLibCVersion(2, 28, 0);
     }
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const sqlite = b.addStaticLibrary("sqlite", null);
+    const sqlite = b.addStaticLibrary(.{
+        .name = "sqlite",
+        .target = target,
+        .optimize = optimize,
+    });
     sqlite.addCSourceFile("third_party/zig-sqlite/c/sqlite3.c", &[_][]const u8{
         "-std=c99",
         "-DSQLITE_ENABLE_JSON1",
     });
-    sqlite.setTarget(target);
-    sqlite.setBuildMode(mode);
     sqlite.addIncludePath("third_party/zig-sqlite/c");
     sqlite.linkLibC();
 
-    const exe = b.addExecutable("zig-sqlite-demo", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const exe = b.addExecutable(.{
+        .name = "zig-sqlite-demo",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.linkLibrary(sqlite);
     exe.addIncludePath("third_party/zig-sqlite/c");
-    exe.addPackagePath("sqlite", "third_party/zig-sqlite/sqlite.zig");
-    exe.install();
+    exe.addAnonymousModule("sqlite", .{
+        .source_file = .{ .path = "third_party/zig-sqlite/sqlite.zig" },
+    });
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
