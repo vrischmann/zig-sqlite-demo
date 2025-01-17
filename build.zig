1921d7844fe9a0d4fc5edff83051d73fd6b5e581
Vincent Rischmann <vincent@rischmann.fr>
2025-01-18 00:18:37 +0100

    build: update for latest zig

--- build.zig
+++ build.zig
@@ -4,18 +4,22 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const sqlite = b.dependency("sqlite", .{
+    const sqlite_dep = b.dependency("sqlite", .{
         .target = target,
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "zig-sqlite-demo",
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.root_module.addImport("sqlite", sqlite.module("sqlite"));
+    exe_mod.addImport("sqlite", sqlite_dep.module("sqlite"));
+
+    const exe = b.addExecutable(.{
+        .name = "zig-sqlite-demo",
+        .root_module = exe_mod,
+    });
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
